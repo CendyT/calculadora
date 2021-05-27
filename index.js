b092f5195b559d837f4e5a96ac04b620d1cf01e6
Raphael Dias <42981400+Rapha-Dias@users.noreply.github.com>
2021-05-27 19:18:10 -0300

    Calculadora
    adicionado soma

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-var valor
-var valor1
-var oper
-var readlineSync = require('readline-sync');
-oper = readlineSync.question("Qual operacao deseja efetuar (+) (-) (*) (/)? : \n");
-valor = parseFloat(readlineSync.question("Insira o primeiro numero: \n"));
-valor1 = parseFloat(readlineSync.question("Insira o segundo numero: \n"));
+let n1 = 10;
+let n2 =20;
+let soma = n1 + n2;
+console.log(soma);
 
-function doOperation(operator, value1, value2) {
-    if (operator == "+") {
-        return value1 + value2;
-    } else if
-        (operator == "-") {
-        return value1 - value2;
-    } else if
-        (operator == "*") {
-        return value1 * value2;
-    } else if
-        (operator == "/") {
-        return value1 / value2;
-    } else {
-        throw new Error('Operação inválida');
-    }
-}
-console.log('O resultado é ', doOperation(oper, valor, valor1)) 
+//Raphael Dias | Soma
+
+
+
