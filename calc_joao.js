26890265c63fcec7ee8b70f2f0f10459d6e8d8ed
João Claudio <jcdias1141@gmail.com>
2021-05-27 18:30:01 -0300

    adicionando calc-joao

---
+++ calc_joao.js
@@ -0,0 +1,8 @@
+
+    var totalTanque =   40;
+    var caminhoComGasolina = 480;
+
+    var caminhoComAlcool = 300;
+
+    document.write("Consumo com Gasolina é " + caminhoComGasolina/totalTanque + " KM/L <br>");
+    document.write("Consumo com Álcool é " + caminhoComAlcool/totalTanque + " KM/L");
